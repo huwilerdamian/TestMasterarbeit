c31daa5542eb66a48ae086aaa52a1649c2468e4b
huwilerdamian <damian.huwiler@hotmail.ch>
2025-07-27 17:36:00 +0000

    fix: LangChain-Import für ChatOpenAI auf Community-Version aktualisiert

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 
